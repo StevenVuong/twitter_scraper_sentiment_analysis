0d53a55258cc330f51600826d2f9bb7c2982fe5a
Steven Vuong <steven@hackpartners.com>
2020-05-10 16:21:50 +0100

    updaet scraper

--- src/web_scraper/tweet_scraper.py
+++ src/web_scraper/tweet_scraper.py
@@ -2,6 +2,8 @@ import tweepy
 import pandas as pd
 import logging 
 import sys
+import csv
+import datetime
 
 # TODO: Add src to path
 sys.path.append("../")
@@ -22,7 +24,7 @@ def read_twitter_access_keys(csv_filepath: str) -> dict:
     return dict(names_keys_matrix)
 
 
-def get_twitter_api_object(twitter_keys_dict: dict):
+def get_twitter_api_object(twitter_keys_dict: dict) -> tweepy.API:
     """ Authenticate our twitter dev credentials and create an api object.
     Also verifies credentials.
     Args:
@@ -59,33 +61,66 @@ def get_twitter_api_object(twitter_keys_dict: dict):
     return api
 
 
-def extract_tweets_to_csv():
-    """For a given 
+# TODO: Set config for csv savepath and cursor options
+def extract_query_to_csv(
+    twitter_api:tweepy.API, 
+    query:str, 
+    csv_savepath:str,
+    counts:int = 100,
+    max_tweets:int= 200
+    ):
+    """For a given query string, searches twitter and extracts items related
+    Then saves to csvfile.
+    Args:
+        - twitter_api(tweepy.API): Authenticated witter API Object
+        - query(str): Matching query to search, E.g.: https://twitter.com/<QUERY>
+        - csv_savepath(str)
+        - counts(int): Default number of retries to attempt when err occurs
+        - max_tweets(int): How many tweets to retrieve
+
+    Refs: 
+        - https://gist.github.com/vickyqian/f70e9ab3910c7c290d9d715491cde44c
+        - Twitter Object: https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
     """
-
-    # Ref: https://gist.github.com/vickyqian/f70e9ab3910c7c290d9d715491cde44c
-
     # Open/Create a file to append data
-    csvFile = open('tweets.csv', 'a')
-    #Use csv Writer
-    csvWriter = csv.writer(csvFile)
+    csv_file = open(csv_savepath, 'a')
+    csv_writer = csv.writer(csv_file)
+    header = ["a"]
+    csv_writer.writerow(header)
 
-    # TODO: What is this actually doing?
+    # Only able to extract tweets 7 days ago
+    week_ago = datetime.date.today() - datetime.timedelta(days=7)
+    formatted_week_ago = "{0}-{1:02d}-{2:02d}".format(
+        week_ago.year,
+        week_ago.month, 
+        week_ago.day
+        )
+
+    # Extract tweets to csv
     for tweet in tweepy.Cursor(
-        api.search, 
-        q=query_hashtag, 
+        twitter_api.search, 
         count=100,
         lang="en",
-        since="2019-06-01"
-        ).items():
-        print (tweet.created_at, tweet.text)
-        csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+        since=formatted_week_ago
+        ).items(max_tweets):
+
+            print(tweet.created_at, tweet.text)
+            tweet.user.description
+            tweet.user.followers_count
+            tweet.user.statuses_count
+            tweet.user.created_at
+            tweet.ue
+            #csv_writer.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+    
+    csv_file.close()
 
 
 def main():
 
     twitter_keys_dict = read_twitter_access_keys("./twitter_keys/twitter_keys.csv")
     twitter_api = get_twitter_api_object(twitter_keys_dict)
+
+    extract_query_to_csv(twitter_api, "COVID19", "/Users/steven/Downloads/tweets.csv")
 
 
 if __name__ == '__main__':
